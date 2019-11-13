1497bebd6773d18bafcaa58e630038f953d38c2e
Daniel Mayle <dmayle012@gmail.com>
2019-11-13 23:07:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
 	    :railroads => {
 	      :pieces => 4,
 	      :rent_in_dollars => {
-	        :names => "value",
+	        :one_piece_owned => "25",
 	        :key2 => "value",
 	        :key3 => "value",
 	        :key4 => "value"
