023cfd1ae61d91999faf4ddf4013d9111f36e074
Daniel Mayle <dmayle012@gmail.com>
2019-11-13 23:15:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
 	      :pieces => 4,
 	      
 	      :rent_in_dollars => {
-	        :one_piece_owned => "25",
+	        :one_piece_owned => 25,
 	        :key2 => "value",
 	        :key3 => "value",
 	        :key4 => "value"
